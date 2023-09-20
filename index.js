347586448468e0b7f4fd4e91b4aeab2d306c3417
Victor <victor.kyalo@student.moringaschool.com>
2023-09-20 12:44:37 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 100); 
+
+const num3 = 28;
+const num4 = 6;
+const mod = num3 % num4;
+
+const numbers = [2, 4, 6, 8, 20, 16];
+const max = Math.max(...numbers);
